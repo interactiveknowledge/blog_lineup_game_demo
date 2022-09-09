74d614e138e41e9711f7e14c1ae10a80201f6b60
Rosemary Stanley <rosemary@interactiveknowledge.com>
2022-09-09 13:10:33 -0400

    Fix static subdirectory for blog game page

--- precache-manifest.49266bf4418271cf159a46db71bf3fd9.js
+++ precache-manifest.49266bf4418271cf159a46db71bf3fd9.js
@@ -5,50 +5,50 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "b4e961b8ac1cc7db129d",
-    "url": "/static/css/2.dccc335b.chunk.css"
+    "url": "/blog_lineup_game_demo/static/css/2.dccc335b.chunk.css"
   },
   {
     "revision": "4177097892a3496c324e",
-    "url": "/static/css/main.610d3f07.chunk.css"
+    "url": "/blog_lineup_game_demo/static/css/main.610d3f07.chunk.css"
   },
   {
     "revision": "b4e961b8ac1cc7db129d",
-    "url": "/static/js/2.4b622fba.chunk.js"
+    "url": "/blog_lineup_game_demo/static/js/2.4b622fba.chunk.js"
   },
   {
     "revision": "4177097892a3496c324e",
-    "url": "/static/js/main.b7d42919.chunk.js"
+    "url": "/blog_lineup_game_demo/static/js/main.b7d42919.chunk.js"
   },
   {
     "revision": "07bed6fddf239279589f",
-    "url": "/static/js/runtime-main.6a160fa4.js"
+    "url": "/blog_lineup_game_demo/static/js/runtime-main.6a160fa4.js"
   },
   {
     "revision": "d2b512929d0ce129771afa96b56a5920",
-    "url": "/static/media/cloud-2.d2b51292.png"
+    "url": "/blog_lineup_game_demo/static/media/cloud-2.d2b51292.png"
   },
   {
     "revision": "fb4021cb8e4cb8c390e17feedc077bf5",
-    "url": "/static/media/map-1-overlay.fb4021cb.png"
+    "url": "/blog_lineup_game_demo/static/media/map-1-overlay.fb4021cb.png"
   },
   {
     "revision": "df33bcfe09d17587f3e2dd27a8a14de9",
-    "url": "/static/media/map-1.df33bcfe.png"
+    "url": "/blog_lineup_game_demo/static/media/map-1.df33bcfe.png"
   },
   {
     "revision": "f206bd771942c13821857b2d44f78ad8",
-    "url": "/static/media/map-2-overlay.f206bd77.png"
+    "url": "/blog_lineup_game_demo/static/media/map-2-overlay.f206bd77.png"
   },
   {
     "revision": "e5f72aad4f4078426b46703171f5ae3b",
-    "url": "/static/media/map-2.e5f72aad.png"
+    "url": "/blog_lineup_game_demo/static/media/map-2.e5f72aad.png"
   },
   {
     "revision": "64d37b8a3a74be03972a603852bab2c4",
-    "url": "/static/media/map-3-overlay.64d37b8a.png"
+    "url": "/blog_lineup_game_demo/static/media/map-3-overlay.64d37b8a.png"
   },
   {
     "revision": "3ce7a3b490821f8ebf4d2d932de89157",
-    "url": "/static/media/map-3.3ce7a3b4.png"
+    "url": "/blog_lineup_game_demo/static/media/map-3.3ce7a3b4.png"
   }
 ]);
