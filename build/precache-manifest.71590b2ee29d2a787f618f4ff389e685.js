8f838c32f1cd6e422e254c968a5a3a982f09c601
Rosemary Stanley <rosemary@interactiveknowledge.com>
2022-09-09 12:45:12 -0400

    Add build folder

---
+++ build/precache-manifest.71590b2ee29d2a787f618f4ff389e685.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b829f98f94797bdfa7e809081f25cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb20263ebcff65f6bf51",
+    "url": "/static/css/2.dccc335b.chunk.css"
+  },
+  {
+    "revision": "58d48bf194dd66724738",
+    "url": "/static/css/main.610d3f07.chunk.css"
+  },
+  {
+    "revision": "cb20263ebcff65f6bf51",
+    "url": "/static/js/2.9ae1af86.chunk.js"
+  },
+  {
+    "revision": "58d48bf194dd66724738",
+    "url": "/static/js/main.bf39b7d4.chunk.js"
+  },
+  {
+    "revision": "61a89c88770ccf282106",
+    "url": "/static/js/runtime-main.a8512c6d.js"
+  },
+  {
+    "revision": "d2b512929d0ce129771afa96b56a5920",
+    "url": "/static/media/cloud-2.d2b51292.png"
+  },
+  {
+    "revision": "fb4021cb8e4cb8c390e17feedc077bf5",
+    "url": "/static/media/map-1-overlay.fb4021cb.png"
+  },
+  {
+    "revision": "df33bcfe09d17587f3e2dd27a8a14de9",
+    "url": "/static/media/map-1.df33bcfe.png"
+  },
+  {
+    "revision": "f206bd771942c13821857b2d44f78ad8",
+    "url": "/static/media/map-2-overlay.f206bd77.png"
+  },
+  {
+    "revision": "e5f72aad4f4078426b46703171f5ae3b",
+    "url": "/static/media/map-2.e5f72aad.png"
+  },
+  {
+    "revision": "64d37b8a3a74be03972a603852bab2c4",
+    "url": "/static/media/map-3-overlay.64d37b8a.png"
+  },
+  {
+    "revision": "3ce7a3b490821f8ebf4d2d932de89157",
+    "url": "/static/media/map-3.3ce7a3b4.png"
+  }
+]);
